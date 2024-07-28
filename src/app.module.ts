669d1bd3b52113d62157c02547c15ee23ceee011
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-28 01:14:05 +0100

    fix: update data source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './module/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './database/ormConfig';
+import { JwtModule } from '@nestjs/jwt';
+import { PostsModule } from './module/posts/posts.module';
+import { CategoryModule } from './module/category/category.module';
+import { CommentsModule } from './module/comments/comments.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot(dataSourceOptions),
+    JwtModule.register({
+      global: true,
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+    PostsModule,
+    CategoryModule,
+    CommentsModule,
+  ],
   controllers: [],
   providers: [],
 })
