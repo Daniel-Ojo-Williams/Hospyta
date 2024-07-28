a3453e66bbe37579d415f96d5ebe3cea99c7a122
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-28 01:15:08 +0100

    chore: abstract base entity

--- src/shared/AbstractEntity.ts
+++ src/shared/AbstractEntity.ts
@@ -4,9 +4,9 @@ export class AbstractEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ default: 'now' })
-  created_at: Date;
+  @Column({ default: () => 'now()', type: 'timestamptz' })
+  created_at: string;
 
-  @Column({ default: 'now()' })
-  updated_at: Date;
+  @Column({ default: () => 'now()', type: 'timestamptz', onUpdate: 'now()' })
+  updated_at: string;
 }
