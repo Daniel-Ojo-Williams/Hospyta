7eab84d049bd1563e2b303ed1f1cbe0ee797be2f
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-28 01:13:39 +0100

    chore: added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.setGlobalPrefix('api/v1', { exclude: ['auth(.*)'] });
   await app.listen(3000);
 }
 bootstrap();
